4468bd46f4e1c95ebe28688b76b033e3d1c92e22
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2020-02-11 14:55:29 +0000

    added user info vc

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Jonathan Oakes on 11/02/2020.
+//  Copyright © 2020 Jonathan Oakes. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
