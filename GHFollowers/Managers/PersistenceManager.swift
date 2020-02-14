d89396ce82d256197b24014846cd77c07a7758ff
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2020-02-14 09:10:10 +0000

    added persistance manager code

---
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,75 @@
+//
+//  PersistenceManager.swift
+//  GHFollowers
+//
+//  Created by Jonathan Oakes on 14/02/2020.
+//  Copyright © 2020 Jonathan Oakes. All rights reserved.
+//
+
+import Foundation
+
+enum PersistenceActionType {
+    case add, remove
+}
+
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favourites = "favourites"
+    }
+    
+    static func updateWith(favourite: Follower, actionType: PersistenceActionType, completed: @escaping (GFError?) -> Void) {
+        retrieveFavourites { result in
+            switch result {
+            case .success(let favourites):
+                var retrievedFavourites = favourites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavourites.contains(favourite) else {
+                        completed(.alreadyInFavourites)
+                        return
+                    }
+                    
+                    retrievedFavourites.append(favourite)
+                    
+                case .remove:
+                    retrievedFavourites.removeAll { $0.login == favourite.login }
+                }
+                
+                completed(save(favourites: favourites))
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
+    static func retrieveFavourites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        guard let favouritesData = defaults.object(forKey: Keys.favourites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favourites = try decoder.decode([Follower].self, from: favouritesData)
+            completed(.success(favourites))
+        } catch {
+            completed(.failure(.unableToFavourite))
+        }
+    }
+    
+    static func save(favourites: [Follower]) -> GFError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavourites = try encoder.encode(favourites)
+            defaults.set(encodedFavourites, forKey: Keys.favourites)
+            return nil
+        } catch {
+            return .unableToFavourite
+        }
+    }
+}
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -13,4 +13,6 @@ enum GFError: String, Error {
     case unableToComplete = "Unable To Complete Your Request. Please Check Your Internet Connection"
     case invalidResponse = "Invalid Response From The Server. Please Try Again"
     case invalidData = "The Data Received From The Server Was Invalid. Please Try Again."
+    case unableToFavourite = "There Was An Error Favouriting This User. Please Try Again"
+    case alreadyInFavourites = "You've already Favourited This User."
 }
